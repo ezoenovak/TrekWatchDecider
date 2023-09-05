525ef5da1f0bc7f4f2ae09de4b1190b3f617a75a
Zoe Novak <e.zoe.novak@gmail.com>
2023-09-05 20:14:46 +0400

    refactor the code

--- TrekWatchDecider.py
+++ TrekWatchDecider.py
@@ -2,53 +2,27 @@
 # The simplest version just chooses a random episode of all Trek.
 # More complex versions may allow for choosing favorite show, characters, subjects.
 
-# enterprise 4 seasons
-# s1: 26
-# s2: 26
-# s3: 24
-# s4: 22
-# The code is supposed to choose an episode within the range of 4 seasons and within the range of 26 (approx) episodes
-# Let's start with the first season.
-
-# season = random.randint(1, 4)
-# if season == 1 or season == 2:
-#     episode = random.randint(1, 26)
-# elif season == 3:
-#     episode = random.randint(1, 24)
-# else:
-#     episode = random.randint(1, 22)
-# print(f"Let's watch season {season}, episode {episode}!")
-
 
 import random
-shows = ["The Original series",
-         "The Animated Series",
-         "The Next Generation",
-         "Deep Space Nine",
-         "Voyager",
-         "Enterprise",
-         "Discovery",
-         "Short Treks",
-         "Picard",
-         "Lower Decks",
-         "Prodigy",
-         "Strange New Worlds"]
+shows_info = {
+    "The Original Series": {"seasons": 3, "episodes": [29, 26, 24]},
+    "The Animated Series": {"seasons": 2, "episodes": [16, 6]},
+    "The Next Generation": {"seasons": 7, "episodes": [26, 22, 26, 26, 26, 26, 26]},
+    "Deep Space Nine": {"seasons": 7, "episodes": [20, 26, 26, 26, 26, 26, 26]},
+    "Voyager": {"seasons": 7, "episodes": [16, 26, 26, 26, 26, 26, 26]},
+    "Enterprise": {"seasons": 4, "episodes": [26, 26, 24, 22]},
+    "Discovery": {"seasons": 4, "episodes": [15, 14, 13, 13]},
+    "Short Treks": {"seasons": 2, "episodes": [4, 6]},
+    "Picard": {"seasons": 3, "episodes": [10] * 3},
+    "Lower Decks": {"seasons": 3, "episodes": [10] * 3},
+    "Prodigy": {"seasons": 1, "episodes": [20]},
+    "Strange New Worlds": {"seasons": 2, "episodes": [10] * 2}
+}
 
-# def get_random_show():
-#     return random.choice(shows)
-
-show = random.choice(shows)
-print(show)
-
-# get_random_show()
-# season = random.randint(1, 4)
-#
-# if season == 1 or season == 2:
-#     max_episode = 26
-# elif season == 3:
-#     max_episode = 24
-# else:
-#     max_episode = 22
-#
-# episode = random.randint(1, max_episode)
-# print(f"Let's watch season {season}, episode {episode}!")
+show = random.choice(list(shows_info.keys()))
+if show in shows_info:
+    show_info = shows_info[show]
+    season = random.randint(1, show_info["seasons"])
+    max_episode = show_info["episodes"][season - 1]
+    episode = random.randint(1, max_episode)
+    print(f"Let's watch season {season}, episode {episode} of {show}!")
